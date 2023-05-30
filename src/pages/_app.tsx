6604926fdff98e1337f395f18b32cc566a399c26
andrei1994rus <rusakov.andrei@hotmail.com>
2023-05-30 22:42:25 +0300

    add custom font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,20 @@
 import '@/styles/globals.css';
 import type {AppProps} from 'next/app';
 import dynamic from 'next/dynamic';
+import {Roboto} from '@next/font/google';
+
+const roboto = Roboto({
+	subsets: ['cyrillic', 'latin'],
+	weight: ['400', '500'],
+});
 
 const NavigationBar = dynamic(() => import('@/components/navigationBar'));
 
 export default function App({Component, pageProps}: AppProps) {
 	return (
-		<>
+		<div className={roboto.className}>
 			<NavigationBar />
 			<Component {...pageProps} />
-		</>
+		</div>
 	);
 }
